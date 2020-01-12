ea02809124a2140cda5284f0a3f32b1b0fe5e4c6
cry0this <cry0this@gmail.com>
2020-01-13 01:01:13 +0300

    add nginx

--- docker/novnc/entrypoint.sh
+++ docker/novnc/entrypoint.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-if [[ -f /etc/cert.pem ]]; then 
-  /app/utils/launch.sh --vnc ${VNC_HOST}:${VNC_PORT} --listen ${NOVNC_PORT} --cert /etc/cert.pem --ssl-only
-else 
-  /app/utils/launch.sh --vnc ${VNC_HOST}:${VNC_PORT} --listen ${NOVNC_PORT} --cert
-fi
+/app/utils/launch.sh --vnc ${VNC_HOST}:${VNC_PORT} --listen ${NOVNC_PORT} 
