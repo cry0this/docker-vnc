3d746c90a99aa975f0c5b16c66954ecbf5108426
cry0this <cry0this@gmail.com>
2020-01-13 21:01:59 +0300

    replace openbox with tinywm

--- docker/tigervnc/entrypoint.sh
+++ docker/tigervnc/entrypoint.sh
@@ -4,4 +4,4 @@ if [[ ! -d /home/ubuntu/.vnc ]]; then
 fi
 vncserver -kill :1
 echo "${VNC_PASSWORD}" | vncpasswd -f > /home/ubuntu/.vnc/passwd
-vncserver :1 -depth ${VNC_COL_DEPTH} -geometry ${VNC_RESOLUTION} -cleanstale -localhost no -fg -SecurityTypes VNCAuth,TLSVnc -passwd /home/ubuntu/.vnc/passwd -xstartup openbox-session
+vncserver :1 -depth ${VNC_COL_DEPTH} -geometry ${VNC_RESOLUTION} -cleanstale -localhost no -fg -SecurityTypes VNCAuth,TLSVnc -passwd /home/ubuntu/.vnc/passwd -xstartup tinywm
