0693cab22e8fcbe1ce0808af4b36741b20956e58
cry0this <cry0this@gmail.com>
2020-01-17 08:21:25 +0300

    experimental hls audio support

--- docker/tigervnc/entrypoint.sh
+++ docker/tigervnc/entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+pulseaudio -n -v \
+            --load=module-native-protocol-unix \
+            --load=module-always-sink \
+            --start
+
+ffmpeg -f pulse -i default -fflags nobuffer -ac 1 -c:a aac -ar 22050 -ab 128k -f flv rtmp://nginx:1935/stream/audio &> /dev/null &
+
 if [[ ! -d /home/ubuntu/.vnc ]]; then 
   mkdir /home/ubuntu/.vnc
 fi
